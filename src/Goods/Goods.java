a865ebb83b37762e93b4accdf76f4a0c4ebe1f96
Fantasia <1002238393@qq.com>
2017-04-26 10:19:49 +0800

    add Goods.java and GoodsDAO.java change the packages

---
+++ src/Goods/Goods.java
@@ -0,0 +1,110 @@
+package Goods;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by Fantasia on 2017/4/25.
+ */
+public class Goods {
+    private String Id;
+    private String name;
+    private String type;
+    private String fineness;
+    private String description;
+    private String pictures;
+    private String ownerId;
+    private Date dateChanged;
+    private int state;
+    private String borrowerId;
+    private Date dateReturn;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFineness() {
+        return fineness;
+    }
+
+    public void setFineness(String fineness) {
+        this.fineness = fineness;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(String pictures) {
+        this.pictures = pictures;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Date getDateChanged() {
+        return dateChanged;
+    }
+
+    public void setDateChanged(Date dateChanged) {
+        this.dateChanged = dateChanged;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getBorrowerId() {
+        return borrowerId;
+    }
+
+    public void setBorrowerId(String borrowerId) {
+        this.borrowerId = borrowerId;
+    }
+
+    public Date getDateReturn() {
+        return dateReturn;
+    }
+
+    public void setDateReturn(Date dateReturn) {
+        this.dateReturn = dateReturn;
+    }
+}
+
--- web/addGoods.jsp
+++ web/addGoods.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
-  User: haoyun
-  Date: 2017/4/21
-  Time: 20:59
+  User: Fantasia
+  Date: 2017/4/26
+  Time: 10:02
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>首页</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>添加商品</title>
+</head>
+<body>
+<form action=""></form>
+</body>
 </html>
